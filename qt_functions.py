f78a3cdb9df2e874d2ff695991b4461b7fb0ce91
DESKTOP-NVNRDH6\KM <mineevkv@gmail.com>
2024-11-05 15:36:24 +0300

    Stable version (after first real test)

--- qt_functions.py
+++
@@ -1,8 +0,0 @@
-"""
-Module for defining external functions to use in Qt-interface
-"""
-
-def set_text(label, text, color = 'red'):
-    """Set label text: (obj, str, str)"""
-    label.setText(text)
-    label.setStyleSheet(f'color: {color}')
